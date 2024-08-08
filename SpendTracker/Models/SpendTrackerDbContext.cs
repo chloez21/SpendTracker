98e6bae2e7c101e9316aee9ec80d1b79f7016c99
czhu1 <czhu1@WINDOWS>
2024-08-08 16:54:10 -0400

    Add project files.

---
+++ SpendTracker/Models/SpendTrackerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendTracker.Models
+{
+    public class SpendTrackerDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public SpendTrackerDbContext(DbContextOptions<SpendTrackerDbContext> options)
+            : base(options)
+        {
+        
+        }
+    }
+}
